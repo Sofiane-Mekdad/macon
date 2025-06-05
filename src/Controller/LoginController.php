852eefb580b69fdd3de4d3466c0c0cf289ac0431
Sofiane <sofiane.mekdad59@gmail.com>
2025-06-05 16:23:24 +0200

    d

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\AppBundle\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,16 +14,45 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 final class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-     public function index(AuthenticationUtils $authenticationUtils): Response
-      {
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
          // get the login error if there is one
-         $error = $authenticationUtils->getLastAuthenticationError();
+        $error = $authenticationUtils->getLastAuthenticationError();
 
          // last username entered by the user
-         $lastUsername = $authenticationUtils->getLastUsername();
+        $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('login/index.html.twig', [
-             'last_username' => $lastUsername,
-             'error'         => $error,
+            'last_username' => $lastUsername,
+            'error'         => $error,
         ]);
+    }
+
+    #[Route('/signup', name: 'app_signup')]
+    public function registerAction(Request $request, EntityManagerInterface $em)
+    {
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+
+            // 4) save the User!
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('app_service');
+        }
+
+        return $this->render(
+            'login/signup.html.twig',
+            array('form' => $form->createView())
+        );
     }
 }
